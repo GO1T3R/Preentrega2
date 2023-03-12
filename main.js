05633623b7af18563b4b089465ed52792412f975
GO1T3R <eduardo_1999sosa@hotmail.com>
2023-03-11 21:05:03 -0500

    Subir a github

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
-let refresh = document.getElementById('refresh'); //boton para recargar la pagina
-refresh.addEventListener('click', _ => {
-            location.reload();
-})
-
-
 //declarar y asignararles un valor mediante un input
 let nombre = prompt("Ingrese el nombre del alumno: ");
 
-let n1 = parseInt(prompt("Ingrese una nota: "));
-let n2 = parseInt(prompt("Ingrese la siguiente nota: "));
-let n3 = parseInt(prompt("Ingrese la ultima nota"));
+// materias y califcaciones
+let m1 = prompt("Ingrese el nombre de la primera materia");
+let n1 = parseInt(prompt(`Ingrese la nota de la materia: ${m1}`));
 
+let m2 = prompt("Ingrese el nombre de  la segunda materia");
+let n2 = parseInt(prompt(`Ingrese la nota de la materia: ${m2}`));
+
+let m3 = prompt("Ingrese el nombre de la tercera materia");
+let n3 = parseInt(prompt(`Ingrese la nota de la materia: ${m3}`));
+
+// Se almacena los datos en un array
+materiasCalificaciones = [
+    {materia: m1, calificacion: n1},
+    {materia: m2, calificacion: n2},
+    {materia: m3, calificacion: n3},
+]
 
 //Funcion para sacar el promedio final de la calificacion
 function SacarPromedio(){
     promedio = (n1 + n2 + n3) / 3;        
     return promedio.toFixed(2);
-
     }
+
+//Filtrar calificaciones
+let reprobadas;
+reprobadas = materiasCalificaciones.filter( promedio => promedio.calificacion < 7);
 
 //condicional si el alumno esta aprovado o reprobado    
 if(SacarPromedio() <= 10 && SacarPromedio() >= 7){
     alert("El alumno " + nombre + " ha aprovado con: " + SacarPromedio());
 }else{
     alert("has reprobado :(");
+    alert(`Las materias que has reprobado son: ${JSON.stringify(reprobadas)}`);
 }
